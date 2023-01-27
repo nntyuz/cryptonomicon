7148217b2a37d81c6ed504de0dbe567d7318a01f
nnuyz <hellonedesing@gmail.com>
2023-01-27 19:54:51 +0800

    #7 move logic to computed and watch

--- src/App.vue
+++ src/App.vue
@@ -52,8 +52,8 @@
         </div>
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-          <div v-for="t in filteredTickers()" :key="t.name" @click="select(t)" :class="{
-            'border-4': sel === t,
+          <div v-for="t in paginatedTickers" :key="t.name" @click="select(t)" :class="{
+            'border-4': selectedTicker === t,
           }" class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer">
             <div class="px-4 py-5 sm:p-6 text-center">
               <dt class="text-sm font-medium text-gray-500 truncate">
@@ -77,15 +77,15 @@
         </dl>
         <hr class="w-full border-t border-gray-600 my-4" />
       </template>
-      <section v-if="sel" class="relative">
+      <section v-if="selectedTicker" class="relative">
         <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          {{ sel.name.toUpperCase() }} - USD
+          {{ selectedTicker.name.toUpperCase() }} - USD
         </h3>
         <div class="flex items-end border-gray-600 border-b border-l h-64">
-          <div v-for="(bar, idx) in normalizeGraph()" :key="idx" :style="{ height: `${bar}%` }"
+          <div v-for="(bar, idx) in normalizedGraph" :key="idx" :style="{ height: `${bar}%` }"
             class="bg-purple-800 border w-10"></div>
         </div>
-        <button @click="sel = null" type="button" class="absolute top-0 right-0">
+        <button @click="selectedTicker = null" type="button" class="absolute top-0 right-0">
           <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
             xmlns:svgjs="http://svgjs.com/svgjs" version="1.1" width="30" height="30" x="0" y="0"
             viewBox="0 0 511.76 511.76" style="enable-background: new 0 0 512 512" xml:space="preserve">
@@ -108,15 +108,16 @@ export default {
   data() {
     return {
       ticker: "",
+      filter: "",
+
       tickers: [],
-      sel: null,
+      selectedTicker: null,
+
       graph: [],
       coinList: [],
       intervals: {},
       isSame: false,
       page: 1,
-      filter: "",
-      hasNextPage: true
     };
   },
 
@@ -144,6 +145,45 @@ export default {
     filteredCoins() {
       return this.coinList.map(a => a.Symbol).filter(a => a.toLowerCase().includes(this.ticker.toLowerCase())).slice(0, 4)
     },
+
+    startIndex() {
+      return (this.page - 1) * 6
+    },
+
+    endIndex() {
+      return this.page * 6;
+    },
+
+    filteredTickers() {
+      return this.tickers.filter(ticker => ticker.name.toUpperCase().includes(this.filter.toUpperCase()));
+    },
+
+    paginatedTickers() {
+      return this.filteredTickers.slice(this.startIndex, this.endIndex);
+    },
+
+    hasNextPage() {
+      return this.filteredTickers.length > this.endIndex;
+    },
+
+    normalizedGraph() {
+      const maxValue = Math.max(...this.graph);
+      const minValue = Math.min(...this.graph);
+
+      if(maxValue === minValue) {
+        return this.graph.map(() => 50)
+      }
+
+      return this.graph.map(
+        (price) => 5 + ((price - minValue) * 95) / (maxValue - minValue)
+      );
+    },
+    pageStateOption() {
+      return {
+        filter: this.filter,
+        page: this.page
+      }
+    }
   },
 
   mounted() {
@@ -168,17 +208,6 @@ export default {
       }
     },
 
-    filteredTickers() {
-      const start = (this.page - 1) * 6
-      const end = this.page * 6;
-
-      const tickers = this.tickers.filter(ticker => ticker.name.toUpperCase().includes(this.filter.toUpperCase()));
-
-      this.hasNextPage = tickers.length > end;
-
-      return tickers.slice(start, end);
-    },
-
     subscribeToUpdate(tickerName) {
       this.intervals[this.ticker] = setInterval(async () => {
         const f = await fetch(
@@ -188,8 +217,8 @@ export default {
 
         this.tickers.find(t => t.name === tickerName).price =
           data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
-        if (this.sel?.name === tickerName) {
-          //this.sel?.name === [ this.sel && this.sel.name ]
+        if (this.selectedTicker?.name === tickerName) {
+          //this.selectedTicker?.name === [ this.selectedTicker && this.selectedTicker.name ]
           this.graph.push(data.USD);
         }
       }, 5000);
@@ -201,38 +230,43 @@ export default {
         name: this.ticker,
         price: "-",
       };
-      this.tickers.push(currentTicker);
-      localStorage.setItem('cryptonomicon-list', JSON.stringify(this.tickers));
+      this.tickers = [...this.tickers, currentTicker];
       this.subscribeToUpdate(currentTicker.name);
       this.filter = "";
     },
 
     select(ticker) {
-      this.sel = ticker;
-      this.graph = [];
+      this.selectedTicker = ticker;
     },
 
     handleDelete(tickerToRemove) {
       this.tickers = this.tickers.filter(t => t !== tickerToRemove);
       clearInterval(this.intervals[tickerToRemove.name]);
-    },
-
-    normalizeGraph() {
-      const maxValue = Math.max(...this.graph);
-      const minValue = Math.min(...this.graph);
-      return this.graph.map(
-        (price) => 5 + ((price - minValue) * 95) / (maxValue - minValue)
-      );
+      if(this.selectedTicker === tickerToRemove) {
+        this.selectedTicker = null
+      }
     },
   },
   watch: {
-    filter() {
-      this.page = 1;
-      window.history.pushState(null, document.title, `${window.location.pathname}?filter=${this.filter}&page=${this.page}`)
+    tickers() {
+      localStorage.setItem('cryptonomicon-list', JSON.stringify(this.tickers));
+    },
+    selectedTicker() {
+      this.graph = [];
     },
 
-    page() {
-      window.history.pushState(null, document.title, `${window.location.pathname}?filter=${this.filter}&page=${this.page}`)
+    paginatedTickers() {
+      if(this.paginatedTickers.length === 0 && this.page > 1) {
+        this.page -= 1
+      }
+    },
+
+    filter() {
+      this.page = 1;
+    },
+
+    pageStateOption(value) {
+      window.history.pushState(null, document.title, `${window.location.pathname}?filter=${value.filter}&page=${value.page}`)
     }
   }
 };
